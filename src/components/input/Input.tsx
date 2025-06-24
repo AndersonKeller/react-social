39ec73276938b37909e6d662aad7ad6ac0e3cfb0
AndersonKeller <andersonkellervendas@hotmail.com>
2025-06-24 19:48:27 -0300

    add iconify input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import { Iconify } from "../iconify/Iconify"
 import style from "./style.module.css"
 interface InputProps {
     label: string
@@ -7,11 +9,28 @@ interface InputProps {
     register:{}
 }
 export const Input = ({label,type,placeholder,errorMsg,register}:InputProps)=>{
-    return <fieldset>
+    const [newType,setNewType] = useState("password")
+    const changeType = ()=>{
+        if(newType === "password"){
+            setNewType("text")
+        }else{
+            setNewType("password")
+        }
+    }
+    return <fieldset className={style.fieldset}>
         <label htmlFor={label}>{label}</label>
+        {
+            type === "password"?
+            <>
+            <input type={newType} />
+            <Iconify onClick={changeType} className={style.icon} icon="ri:eye-line"/>
+            </>
+        :
         <input {...register} placeholder={placeholder} type={type} id={label}/>
+         }
          {errorMsg ? 
                 <span className={style.error}>{errorMsg}</span>
                 : <span className={style.error}></span> }
+        
     </fieldset>
 }
